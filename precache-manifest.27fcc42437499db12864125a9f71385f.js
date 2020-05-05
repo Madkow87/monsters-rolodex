cac72152df993a20ddf0495dc2d5181b89b8c384
nick gizzi <n.gizzi5@gmail.com>
2020-05-05 19:09:44 -0400

    Updates

---
+++ precache-manifest.27fcc42437499db12864125a9f71385f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bf5bf53c9a3b7db5b232905b6388274",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dd79639d42d16583afb6",
+    "url": "/monsters-rolodex/static/css/main.4df114fd.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd79639d42d16583afb6",
+    "url": "/monsters-rolodex/static/js/main.173eb7c7.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
